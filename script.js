0abc052bb184fe28e59aeb225aa711224b1055ce
FarouqII <farouq006@gmail.com>
2025-04-21 18:54:49 +0300

    Support more than single-digit and set some rules
    Support more than single-digit operations
        - Separated the write to screen functions between numbers and operators
    Added rule to prevent consecutive operators
    Added rule to prevent operator in the beginning of an operation

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ const numbers = document.querySelectorAll(".number");
 let equalButton = false;
 let currentEquation = [];
 
-operators.forEach(button => button.addEventListener("click", () => addToScreen(button.textContent)));
-numbers.forEach(button => button.addEventListener("click", () => addToScreen(button.textContent)));
+operators.forEach(button => button.addEventListener("click", () => operatorButtons(button.textContent)));
+numbers.forEach(button => button.addEventListener("click", () => numberButtons(button.textContent)));
 equals.addEventListener("click", () => {
     if (screen.textContent == "") alert("Display empty!");
     else calculate(currentEquation[0], currentEquation[2], currentEquation[1]);
@@ -29,19 +29,40 @@ dlt.addEventListener("click", () => {
     currentEquation.forEach(item => screen.textContent += item);
 })
 
+function numberButtons(buttonContent) {
+    if (currentEquation.length == 0 || isNaN(Number(currentEquation[currentEquation.length - 1]))) {
+        currentEquation.push(Number(buttonContent));
+    } else {
+        let last = currentEquation[currentEquation.length - 1];
+    currentEquation[currentEquation.length - 1] = parseInt(String(last) + String(Number(buttonContent)));
+    }
+    screen.textContent += buttonContent;
+    console.log(currentEquation);
+    return;
+}
 
-function addToScreen(buttonContent) {
+function operatorButtons(buttonContent) {
+    let last = currentEquation[currentEquation.length - 1];
+    // Edge cases
+    if (currentEquation.length == 0) {
+        alert("First element must be a number!");
+        return;
+    }
+    if (isNaN(parseInt(last))) {
+        console.log(last);
+        alert("Can't put two consecutive operators!");
+        return;
+    }
+
+    // Main operation
     if (currentEquation.length == 3) {
         calculate(currentEquation[0], currentEquation[2], currentEquation[1]);
-        if (isNaN(parseInt(buttonContent))) currentEquation.push(buttonContent);
-        else currentEquation.push(parseInt(buttonContent));
+        currentEquation.push(buttonContent);
         screen.textContent += buttonContent;
         return;
     } else {
-        if (isNaN(parseInt(buttonContent))) currentEquation.push(buttonContent);
-        else currentEquation.push(parseInt(buttonContent));
+        currentEquation.push(buttonContent);
         screen.textContent += buttonContent;
-        console.log(currentEquation);
     }
 }
 
